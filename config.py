ef670abdaf4cd00c98629983d98ae74a815927ec
kulsumummi <kulsum.aiml.rymec@gmail.com>
2026-09-23 20:09:37 +0530

    Disable DEBUG in production and force host 0.0.0.0

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
 MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'root')
 MYSQL_DB = os.environ.get('MYSQL_DB', 'shieldlink_db')
 
-# Debug mode setting
-DEBUG = True
+# Debug mode setting (Default False for production)
+DEBUG = os.environ.get('FLASK_DEBUG', 'False').lower() in ('true', '1')
